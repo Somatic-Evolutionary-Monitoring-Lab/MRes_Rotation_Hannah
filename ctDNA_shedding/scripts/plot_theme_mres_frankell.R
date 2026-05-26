2346b40ab1f5ac5d38088461fbfb9854c4752eac
Hannah Bazin <baz1hannah@gmail.com>
2026-05-26 16:10:28 +0100

    Update theme

--- ctDNA_shedding/scripts/plot_theme_mres_frankell.R
+++ ctDNA_shedding/scripts/plot_theme_mres_frankell.R
@@ -18,21 +18,21 @@ horiz_line_col <- "#D4DCE8"
 # Custom theme
 # -----------------------------------------------------------------------------
 
-theme_mres_frankell <- function(base_size = 14) {
+theme_mres_frankell <- function(base_size = 16) {
   theme_cowplot(font_size = base_size) %+replace%
     theme(
+      # Font family
+      text              = element_text(colour = "#2D3436", family = "DejaVu Sans"),
       # Title
-      plot.title        = element_text(face = "bold", size = 12, colour = "#2D3436"),
+      plot.title        = element_text(face = "bold", size = 12, colour = "#2D3436", family = "DejaVu Sans"),
       # Axis text and labels
-      axis.text         = element_text(colour = "#2D3436", size = 12),
-      axis.title        = element_text(colour = "#2D3436", size = 14),
+      axis.text         = element_text(colour = "#2D3436", size = 14, family = "DejaVu Sans"),
+      axis.title        = element_text(colour = "#2D3436", size = 16, family = "DejaVu Sans"),
       # Ticks
       axis.ticks        = element_line(colour = "#2D3436", linewidth = 0.8),
       axis.ticks.length = unit(4, "pt"),
       # Legend
-      legend.text       = element_text(colour = "#2D3436"),
-      legend.title      = element_text(colour = "#2D3436"),
-      # General text
-      text              = element_text(colour = "#2D3436")
+      legend.text       = element_text(colour = "#2D3436", family = "DejaVu Sans"),
+      legend.title      = element_text(colour = "#2D3436", family = "DejaVu Sans")
     )
 }
